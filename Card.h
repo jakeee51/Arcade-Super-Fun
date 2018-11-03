ab84e5d98f723edaa0418e10cc3cdf1efeb5c4d2
adavidoff8 <43143528+adavidoff8@users.noreply.github.com>
2018-11-03 16:16:31 -0400

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,1 +1,25 @@
+#pragma once
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// suit: H, C, D, S
+// rank: A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
+
+class card
+{
+public:
+	card();
+	card(int cardFace, int cardSuit);
+	~card();
+	string getValue() { return value; }
+	
+	void printCard();
+
+private:
+	string nameFace;
+	string nameSuit;
+	string value;
+};
